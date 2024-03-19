28f2d14ea4e718bd20570295c1a1a99836f4d498
Marceli <129780388+Marceli-Chlebowski@users.noreply.github.com>
2024-03-19 15:09:34 +0100

    nfdhg

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp1;
 
-Console.WriteLine("Hello, World!");
+var container = new Container(10.0)
+{
+Cargoweight = 12.0
+};
+
+Container Lcontainer = new LiquidContainer(10);
+container.Load(20);
+Lcontainer.Load(30);
+
+    int? a = 1;
+a = null;
+
+Exception? ex = new Exception();
+ex = null;
+
+Console.WriteLine(ex.Message);
